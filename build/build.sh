dfaad7d4d4a7e7d9f5025acbf66296ec1694522f
Erich Whitney <ecwhitney@icloud.com>
2020-10-23 20:57:41 -0400

    still working to improve the build

--- build/build.sh
+++ build/build.sh
@@ -1,35 +1,42 @@
 #!/bin/zsh
 
+src_dir="../src";
 dist_path="../bin/MacOSX";
-work_path="./MacOSX";
+work_path="../build/MacOSX";
+spec_path="../build/spec";
+
+cd ${src_dir}
 
 if [ ! -d ${dist_path} ]; then
     mkdir -p ${dist_path};
 fi;
 
-if [ ! -d ${wokr_path} ]; then
+if [ ! -d ${work_path} ]; then
     mkdir -p ${work_path};
 fi;
 
-for script in ../src/*.py; do
+if [ ! -d ${spec_path} ]; then
+    mkdir -p ${spec_path};
+fi;
+
+for script in *.py; do
     echo "Building ${script}";
     echo "";
-#    pyinstaller -y -F --distpath ${dist_path} --workpath ${work_path} ${script}
-    pyinstaller -y -F --distpath ${dist_path} --workpath ${work_path} ${script} \
-    --hidden-import pyexcel_xlsx \
-    --hidden-import pyexcel_xlsx.xlsxr \
-    --hidden-import pyexcel_xlsx.xlsxw \
-    --hidden-import pyexcel_io.readers.csvr \
-    --hidden-import pyexcel_io.readers.csvz \
-    --hidden-import pyexcel_io.readers.tsv \
-    --hidden-import pyexcel_io.readers.tsvz \
-    --hidden-import pyexcel_io.writers.csvw \
-    --hidden-import pyexcel_io.writers.csvz \
-    --hidden-import pyexcel_io.writers.tsv \
-    --hidden-import pyexcel_io.writers.tsvz \
-    --hidden-import pyexcel_io.database.importers.django \
-    --hidden-import pyexcel_io.database.importers.sqlalchemy \
-    --hidden-import pyexcel_io.database.exporters.django
+    pyinstaller --noconfirm --onefile --specpath ${spec_path} --distpath ${dist_path} --workpath ${work_path} --hidden-import pyexcel_xls.xls ${script};
+#    --hidden-import pyexcel_xlsx \
+#    --hidden-import pyexcel_xlsx.xlsxr \
+#    --hidden-import pyexcel_xlsx.xlsxw \
+#    --hidden-import pyexcel_io.readers.csvr \
+#    --hidden-import pyexcel_io.readers.csvz \
+#    --hidden-import pyexcel_io.readers.tsv \
+#    --hidden-import pyexcel_io.readers.tsvz \
+#    --hidden-import pyexcel_io.writers.csvw \
+#    --hidden-import pyexcel_io.writers.csvz \
+#    --hidden-import pyexcel_io.writers.tsv \
+#    --hidden-import pyexcel_io.writers.tsvz \
+#    --hidden-import pyexcel_io.database.importers.django \
+#    --hidden-import pyexcel_io.database.importers.sqlalchemy \
+#    --hidden-import pyexcel_io.database.exporters.django \
 done;
 echo "Compiled Programs:";
 ls -l ${dist_path};
